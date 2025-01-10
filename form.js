a5c5a7d7b6ab63d439146836308fecdae86a66a7
karaca.aykut@gmail.com <karaca.aykut@gmail.com>
2025-01-10 21:13:12 +0300

    corrected form

--- form.js
+++ form.js
@@ -3,7 +3,7 @@ var inputLtc = document.getElementById("Fname");
 var inputLtc2 = document.getElementById("Tname");
 
 //var sharebtn = document.getElementById("sharebtn");
-var add = "https://artisartstudio.github.io/BrGrlSptWhll/index.html?"
+var add = "https://artisartstudio.github.io/LvchSpthWhl/index.html?"
 
 inputLtc.addEventListener('keyup',OnKeyUp);
 inputLtc2.addEventListener('keyup',OnKeyUp);
@@ -16,11 +16,12 @@ document.addEventListener("mousedown",function(e){
    var fname = inputLtc.value;
    var tname = inputLtc2.value;
 
-   if(target.contains(inputBtc) && (!inputLtc.value || fname.replace(/\s/g, '').length==0) && (!inputLtc2.value || tname.replace(/\s/g, '').length==0)){
+   if(target.contains(inputBtc)){
+      if((!inputLtc.value || fname.replace(/\s/g, '').length==0) || (!inputLtc2.value || tname.replace(/\s/g, '').length==0)){
       //console.log("click");
       window.alert("Please enter valid names!");
       inputBtc.style.pointerEvents = "none";
-      sharebtn.disabled =true;
+   }
    } 
 });
 var constantNumber = 2;
@@ -31,19 +32,19 @@ function OnKeyUp(e) {
       result = add + "fname=" +result.charAt(0).toUpperCase() + result.slice(1);
    } else {
       result=add + "fname=";
-      //sharebtn.disabled =true;
-      inputBtc.style.pointerEvents = "none";
    }
    var result2 = inputLtc2.value;
    if (result2){
       result = result + "&tname=" +result2.charAt(0).toUpperCase() + result2.slice(1);
    } else {
       result=result+"&tname=";
-      //sharebtn.disabled =true;
-      inputBtc.style.pointerEvents = "none";
    }
-   if (result && result2) {
+   if (inputLtc.value && inputLtc2.value) {
       inputBtc.style.pointerEvents = "auto";
+   } else 
+   {
+      inputBtc.style.pointerEvents = "none";
+
    }
    inputBtc.textContent = result;
    inputBtc.href= result;
